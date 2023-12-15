7871df208d729095de7957a495e1a40f0355ca1e
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2023-12-14 19:00:50 -0500

    Subido Alzadolar.

---
+++ AlzaDolar.py
@@ -0,0 +1,18 @@
+arrayDolar = []
+day = 1
+diff = 0
+ran = 0
+
+n = int(input("Ingrese cantidad de dias.\n"))
+for i in range(n):
+    precio = int(input(f'Ingrese el precio. Dia {i+1}\n'))
+    arrayDolar.append(precio)
+for i in range(n-1):
+    diff = arrayDolar[i+1] - arrayDolar[i]
+    if abs(diff) > abs(ran):
+        ran = diff
+        day = i + 1
+if ran != 0: 
+    print(f'El mayor cambio sucedio del {day} al {day+1} con una diferencia de {abs(ran)} puntos.')
+else:
+    print("No hubo alza.")
