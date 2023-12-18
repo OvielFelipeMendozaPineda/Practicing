2d55e1bbc85c9c3206d5d687f426a47ea58334d1
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2023-12-18 13:24:11 -0500

    Subido desde campus

---
+++ an.py
@@ -0,0 +1,15 @@
+# Escribir una función que calcule si el número ingresado es primo o no. Devolviendo True o False.
+
+
+def calcPrimaridad():
+    num = int(input("Ingrese un numero.\n"))
+    cont = 0
+    for i in range(1, num):
+        if num % i == 0:
+            cont += 1
+    if cont > 2: 
+            print(f'{num} no es primo.')
+    else:
+            print(f'{num} es primo.')
+
+calcPrimaridad()
